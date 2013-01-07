66f994096429763f11bad4985ecab113b4b50acc
Yuriy Buchchenko <ybuchchenko@cogniance.com>
2013-01-07 19:47:56 +0200

    Implemented main dialog within computer and human

--- lib/conversation.rb
+++ lib/conversation.rb
@@ -1,9 +1,53 @@
 class Conversation
   def waiting_for_answer
-    STDIN.gets 
+    STDOUT.print ">"
+    STDIN.gets.chop
   end
 
   def want_to_exit?(answer)
     answer == "exit"
   end
+
+  def start
+    loop do
+      tense, form, pronoun, verb = generate_rand_parts_of_sentence
+      sentence_rus = generate_russian_sentence_by tense, form, pronoun, verb
+      sentence_eng = generate_english_sentence_by tense, form, pronoun, verb
+
+      show_to_companion sentence_rus
+      answer = waiting_for_answer
+      break if want_to_exit? answer
+
+      if answer == sentence_eng
+        congrats
+      else
+        upset sentence_eng
+      end
+    end
+  end
+
+  def generate_russian_sentence_by(tense, form, pronoun, verb)
+    ""
+  end
+
+  def generate_english_sentence_by(tense, form, pronoun, verb)
+    ""
+  end
+
+  def generate_rand_parts_of_sentence
+    ["", "", "", ""]
+  end
+
+  def show_to_companion(message)
+    STDOUT.puts "< " + message
+  end
+
+  def congrats
+    STDOUT.puts "You are right!"
+  end
+
+  def upset(expected_sentence)
+    STDOUT.puts "No, you are wrong! Right is: '#{expected_sentence}'"
+  end
 end
+
--- spec/conversation_spec.rb
+++ spec/conversation_spec.rb
@@ -8,6 +8,15 @@ class Conversation
 end
 
 describe Conversation do
+  let(:conversation) { Conversation.new }
+
+  it "has a congrats function when companion answers correctly" do
+    conversation.respond_to?(:congrats).should be_true
+  end
+
+  it "has a upset function when companion answers incorrectly" do
+    conversation.respond_to?(:upset).should be_true
+  end
 
   context "when finishing" do
     let(:conversation) { Conversation.new }
@@ -17,4 +26,5 @@ describe Conversation do
       conversation.want_to_exit?(answer).should be_true
     end
   end 
+
 end
