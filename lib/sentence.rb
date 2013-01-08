2ba6845590146ea2d8b26b15852e3b7b1c034699
Yuriy Buchchenko <ybuchchenko@cogniance.com>
2013-01-08 11:10:42 +0200

    Added Sentence class like base for all sentences. It has some useful methods for using separately

---
+++ lib/sentence.rb
@@ -0,0 +1,21 @@
+class Sentence
+  def self.tenses
+    [:past, :present, :future]
+  end
+
+  def self.expression_forms
+    [:statement, :question, :negation]
+  end
+
+  def self.pronouns
+    [:i, :thou, :you, :we, :they, :he, :she]
+  end
+
+  def self.verbs
+    [:program, :love, :live]
+  end
+
+  def self.is_he_she?(pronoun)
+    [:he, :she].include? pronoun.to_sym
+  end
+end
---
+++ spec/sentence_spec.rb
@@ -0,0 +1,27 @@
+require_relative 'spec_helper'
+require_relative '../lib/sentence'
+
+describe Sentence do
+  it "can formed with three different tenses" do
+    Sentence.tenses.should == [:past, :present, :future]
+  end 
+
+  it "can stand in three expression forms" do
+    Sentence.expression_forms.should == [:statement, :question, :negation]
+  end
+
+  it "has all known personal pronouns" do
+    Sentence.pronouns.should == [:i, :thou, :you, :we, :they, :he, :she]
+  end
+
+  it "defines personal pronouns is it he or she" do
+    Sentence.is_he_she?(:he).should be_true
+    Sentence.is_he_she?(:she).should be_true
+    Sentence.is_he_she?(:i).should be_false
+    Sentence.is_he_she?(:we).should be_false
+  end
+
+  it "has the set of verbs" do
+    Sentence.verbs.should_not be_empty
+  end
+end
