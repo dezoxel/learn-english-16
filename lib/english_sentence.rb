6cb739bc5f900f3ef14422b00520865f9eaf0120
Yuriy Buchchenko <ybuchchenko@cogniance.com>
2013-01-10 11:43:01 +0200

    Implemented constructing sentence for past tense

--- lib/english_sentence.rb
+++ lib/english_sentence.rb
@@ -1,4 +1,5 @@
 require_relative 'sentence'
+require_relative 'string'
 class EnglishSentence < Sentence
 
   def present_statement_for(pronoun, verb)
@@ -16,5 +17,18 @@ class EnglishSentence < Sentence
     do_does = "do"
     do_does += "es" if self.class.is_he_she? pronoun
     "#{pronoun} #{do_does}n't #{verb}"
+  end
+
+  def past_statement_for(pronoun, verb)
+    verb = verb[0..-2] if verb.ends_with? "e"
+    "#{pronoun} #{verb}ed"
+  end
+
+  def past_question_for(pronoun, verb)
+    "did #{pronoun} #{verb}?"
+  end
+
+  def past_negation_for(pronoun, verb)
+    "#{pronoun} didn't #{verb}"
   end
 end
--- spec/english_sentence_spec.rb
+++ spec/english_sentence_spec.rb
@@ -33,4 +33,34 @@ describe EnglishSentence do
     sentence.present_negation_for("he", "love").should == "he doesn't love"
     sentence.present_negation_for("she", "love").should == "she doesn't love"
   end
+
+  it "#past_statement_for" do
+    sentence.past_statement_for("i", "love").should == "i loved"
+    sentence.past_statement_for("thou", "love").should == "thou loved"
+    sentence.past_statement_for("you", "love").should == "you loved"
+    sentence.past_statement_for("we", "love").should == "we loved"
+    sentence.past_statement_for("they", "love").should == "they loved"
+    sentence.past_statement_for("he", "love").should == "he loved"
+    sentence.past_statement_for("she", "love").should == "she loved"
+  end
+
+  it "#past_question_for" do
+    sentence.past_question_for("i", "love").should == "did i love?"
+    sentence.past_question_for("thou", "love").should == "did thou love?"
+    sentence.past_question_for("you", "love").should == "did you love?"
+    sentence.past_question_for("we", "love").should == "did we love?"
+    sentence.past_question_for("they", "love").should == "did they love?"
+    sentence.past_question_for("he", "love").should == "did he love?"
+    sentence.past_question_for("she", "love").should == "did she love?"
+  end
+
+  it "#past_negation_for" do
+    sentence.past_negation_for("i", "love").should == "i didn't love"
+    sentence.past_negation_for("thou", "love").should == "thou didn't love"
+    sentence.past_negation_for("you", "love").should == "you didn't love"
+    sentence.past_negation_for("we", "love").should == "we didn't love"
+    sentence.past_negation_for("they", "love").should == "they didn't love"
+    sentence.past_negation_for("he", "love").should == "he didn't love"
+    sentence.past_negation_for("she", "love").should == "she didn't love"
+  end
 end
