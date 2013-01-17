13e5071d9125630a1d34af5ddaa3445996c7c7f8
Yuriy Buchchenko <ybuchchenko@cogniance.com>
2013-01-17 08:21:36 +0200

    Completed writing Russian_Sentence class

--- lib/russian_sentence.rb
+++ lib/russian_sentence.rb
@@ -1,26 +1,57 @@
 # coding: utf-8
 require_relative 'sentence'
 require_relative 'string'
-require_relative 'russian_english_map'
 
 class Russian_Sentence < Sentence
 
   def initialize
-    @map = Russian_English_Map.new
+    @verbs = YAML.load_file("./db/verbs.yml")
+    @russian_verbs = YAML.load_file("./db/russian_verbs.yml")
+    @pronouns = YAML.load_file("./db/pronouns.yml")
   end
 
   def present_statement_for(pronoun, verb)
-    verb = @map.russian_verb_by_pronoun(verb, pronoun)
+    verb = @russian_verbs[verb]["настоящее"][pronoun]
     "#{pronoun} #{verb}"
   end
 
   def present_question_for(pronoun, verb)
-    verb = @map.russian_verb_by_pronoun(verb, pronoun)
+    verb = @russian_verbs[verb]["настоящее"][pronoun]
     "#{pronoun} #{verb}?"
   end
 
   def present_negation_for(pronoun, verb)
-    verb = @map.russian_verb_by_pronoun(verb, pronoun)
+    verb = @russian_verbs[verb]["настоящее"][pronoun]
+    "#{pronoun} не #{verb}"
+  end
+
+  def past_statement_for(pronoun, verb)
+    verb = @russian_verbs[verb]["прошлое"][pronoun]
+    "#{pronoun} #{verb}"
+  end
+
+  def past_question_for(pronoun, verb)
+    verb = @russian_verbs[verb]["прошлое"][pronoun]
+    "#{pronoun} #{verb}?"
+  end
+
+  def past_negation_for(pronoun, verb)
+    verb = @russian_verbs[verb]["прошлое"][pronoun]
+    "#{pronoun} не #{verb}"
+  end
+
+  def future_statement_for(pronoun, verb)
+    verb = @russian_verbs[verb]["будущее"][pronoun]
+    "#{pronoun} #{verb}"
+  end
+
+  def future_question_for(pronoun, verb)
+    verb = @russian_verbs[verb]["будущее"][pronoun]
+    "#{pronoun} #{verb}?"
+  end
+
+  def future_negation_for(pronoun, verb)
+    verb = @russian_verbs[verb]["будущее"][pronoun]
     "#{pronoun} не #{verb}"
   end
 end
--- spec/russian_sentence_spec.rb
+++ spec/russian_sentence_spec.rb
@@ -31,4 +31,58 @@ describe Russian_Sentence do
     sentence.present_negation_for("он",  "любить").should == "он не любит"
     sentence.present_negation_for("она", "любить").should == "она не любит"
   end
+
+  it "#past_statement_for" do
+    sentence.past_statement_for("я",   "любить").should == "я любил"
+    sentence.past_statement_for("вы",  "любить").should == "вы любили"
+    sentence.past_statement_for("мы",  "любить").should == "мы любили"
+    sentence.past_statement_for("они", "любить").should == "они любили"
+    sentence.past_statement_for("он",  "любить").should == "он любил"
+    sentence.past_statement_for("она", "любить").should == "она любила"
+  end
+
+  it "#past_question_for" do
+    sentence.past_question_for("я",   "любить").should == "я любил?"
+    sentence.past_question_for("вы",  "любить").should == "вы любили?"
+    sentence.past_question_for("мы",  "любить").should == "мы любили?"
+    sentence.past_question_for("они", "любить").should == "они любили?"
+    sentence.past_question_for("он",  "любить").should == "он любил?"
+    sentence.past_question_for("она", "любить").should == "она любила?"
+  end
+
+  it "#past_negation_for" do
+    sentence.past_negation_for("я",   "любить").should == "я не любил"
+    sentence.past_negation_for("вы",  "любить").should == "вы не любили"
+    sentence.past_negation_for("мы",  "любить").should == "мы не любили"
+    sentence.past_negation_for("они", "любить").should == "они не любили"
+    sentence.past_negation_for("он",  "любить").should == "он не любил"
+    sentence.past_negation_for("она", "любить").should == "она не любила"
+  end
+
+  it "#future_statement_for" do
+    sentence.future_statement_for("я",   "любить").should == "я буду любить"
+    sentence.future_statement_for("вы",  "любить").should == "вы будете любить"
+    sentence.future_statement_for("мы",  "любить").should == "мы будем любить"
+    sentence.future_statement_for("они", "любить").should == "они будут любить"
+    sentence.future_statement_for("он",  "любить").should == "он будет любить"
+    sentence.future_statement_for("она", "любить").should == "она будет любить"
+  end
+
+  it "#future_question_for" do
+    sentence.future_question_for("я",   "любить").should == "я буду любить?"
+    sentence.future_question_for("вы",  "любить").should == "вы будете любить?"
+    sentence.future_question_for("мы",  "любить").should == "мы будем любить?"
+    sentence.future_question_for("они", "любить").should == "они будут любить?"
+    sentence.future_question_for("он",  "любить").should == "он будет любить?"
+    sentence.future_question_for("она", "любить").should == "она будет любить?"
+  end
+
+  it "#future_negation_for" do
+    sentence.future_negation_for("я",   "любить").should == "я не буду любить"
+    sentence.future_negation_for("вы",  "любить").should == "вы не будете любить"
+    sentence.future_negation_for("мы",  "любить").should == "мы не будем любить"
+    sentence.future_negation_for("они", "любить").should == "они не будут любить"
+    sentence.future_negation_for("он",  "любить").should == "он не будет любить"
+    sentence.future_negation_for("она", "любить").should == "она не будет любить"
+  end
 end
